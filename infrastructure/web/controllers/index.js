5bf5a87b15aa57c09f98587ba0928911584aae48
sidahmed <ahorob57@gmail.com>
2021-08-03 04:44:42 +0100

    created the admin post-new-users controller

--- infrastructure/web/controllers/index.js
+++ infrastructure/web/controllers/index.js
@@ -7,13 +7,21 @@ const { addUser } = require('../../../use-cases/index')
 const makeGetSignUp = require('./get-signup')
 const makePostUser = require('./post-signup')
 const makeGetNewOrder = require('./get-new-order')
+const adminMakePostUser = require('./post-new-user')
 
 const getSignUp = makeGetSignUp()
 const postSignup = makePostUser({ addUser, flashMessages, sanitize, validator })
 const getNewOrder = makeGetNewOrder()
+const adminPostUser = adminMakePostUser({
+  addUser,
+  flashMessages,
+  sanitize,
+  validator,
+})
 
 module.exports = {
   getSignUp,
   getNewOrder,
   postSignup,
+  adminPostUser,
 }
