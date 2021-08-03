ad77cc1757803205a56a36495b3a088803b7df52
sidahmed <ahorob57@gmail.com>
2021-08-03 19:47:07 +0100

    added the get-user-orders controller and fixed the use-cases variables

--- infrastructure/web/controllers/get-user-orders.js
+++ infrastructure/web/controllers/get-user-orders.js
@@ -1,7 +1,7 @@
 module.exports = function makeGetUserOrders({ listOrders }) {
   return async function getUserOrders(req, res) {
     try {
-      const ownerId = 'hard-coded-id' // FIXME: remove this line later
+      const ownerId = 'hard-coded-id' // FIXME: remove this line and replace it with an actual ownerId
       const ordersList = await listOrders({ ownerId })
       res.render('admin/orders', { data: { orders: ordersList } })
     } catch (err) {
