1d155a6dbe128a8188b835baab8ddd1edf5fdb63
sidahmed <ahorob57@gmail.com>
2021-08-03 11:39:07 +0100

    renamed the routes method to follow the used convention and passed the store instance instead of the class

--- use-cases/index.js
+++ use-cases/index.js
@@ -12,12 +12,12 @@ const store = new FakeStore({
 const makeAddUser = require('./add-user')
 const makeSignInUser = require('./sign-in-user')
 const makeGetUser = require('./get-user')
-const makeUsersList = require('./list-users')
+const makeGetUsers = require('./list-users')
 
 const getUser = makeGetUser({ usersDB: store })
 const addUser = makeAddUser({ usersDB: store, hashPassword })
 const signInUser = makeSignInUser({ getUser, hashPassword })
-const listUsers = makeUsersList({ usersDB: store })
+const listUsers = makeGetUsers({ usersDB: store })
 
 module.exports = Object.freeze({
   getUser,
