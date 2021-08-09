0d9bff1482d578b4650136dd18d4608f6d0b934d
Abdelaziz Mokhnache <m.abdelaziz1993@gmail.com>
2021-08-09 23:25:30 +0100

    fix admin orders not showing up

--- infrastructure/web/controllers/get-admin-orders.js
+++ infrastructure/web/controllers/get-admin-orders.js
@@ -1,9 +1,7 @@
 module.exports = function makeGetAdminOrders({ listOrders }) {
   return async function getAdminOrders(req, res) {
     try {
-      const ownerId = req.user.id
-
-      const ordersList = await listOrders({ ownerId })
+      const ordersList = await listOrders()
       res.render('admin/orders/index', { data: { orders: ordersList } })
     } catch (err) {
       res.render('admin/orders/new', { errorMessages: [err.message] })
