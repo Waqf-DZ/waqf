3b17326edb5d57ed794ae8c7d9e00979c2d17111
sidahmed <ahorob57@gmail.com>
2021-08-09 21:38:16 +0100

    validate phoneNumber and email fields

--- infrastructure/validator.js
+++ infrastructure/validator.js
@@ -1,11 +1,21 @@
 function isEmailValid(email) {
-  // @todo: will replace this later
-  return email ? true : false
+  const regex = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) //eslint-disable-line
+  const isValidEmail = regex.test(email)
+
+  if (isValidEmail) {
+    return true
+  }
+  return false
 }
 
 function isPhoneValid(phoneNumber) {
-  // @todo: will replace this later
-  return phoneNumber ? true : false
+  const regex = new RegExp(/^(00213|\+213|0)(5|6|7)[0-9]{8}$/) //eslint-disable-line
+  const isValidDzPhone = regex.test(phoneNumber)
+
+  if (isValidDzPhone) {
+    return true
+  }
+  return false
 }
 
 module.exports = {
