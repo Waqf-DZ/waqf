7f171bbd93e763e68d0719117bf2e6416b9605f1
sidahmed <ahorob57@gmail.com>
2021-08-10 09:38:13 +0100

    changed getUser field from id to email

--- use-cases/update-user.js
+++ use-cases/update-user.js
@@ -2,7 +2,7 @@ const makeUser = require('../domain/makeUser')
 
 module.exports = function makeUpdateUser({ usersDB, getUser }) {
   return async function updateUser(userInfo) {
-    const oldUser = await getUser({ id: userInfo.id })
+    const oldUser = await getUser({ email: userInfo.email })
     const user = makeUser(Object.assign({}, oldUser, userInfo))
     const updatedUser = await usersDB.updateUser(user)
     return Promise.resolve(updatedUser)
