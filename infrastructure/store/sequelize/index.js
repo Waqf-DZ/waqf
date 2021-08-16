884d88e0a183de30c6dc18b119358c039b27d457
Abdelaziz Mokhnache <m.abdelaziz1993@gmail.com>
2021-08-16 22:04:46 +0100

    create a productsDB repository

--- infrastructure/store/sequelize/index.js
+++ infrastructure/store/sequelize/index.js
@@ -3,12 +3,15 @@ const { Op } = require('sequelize')
 
 const User = require('./models/user')
 const Order = require('./models/order')
+const Product = require('./models/product')
 
 const makeUsersDB = require('./repositories/usersDB')
 const makeOrdersDB = require('./repositories/ordersDB')
+const makeProductsDB = require('./repositories/productsDB')
 
 module.exports = {
   sequelize,
   usersDB: makeUsersDB({ model: User, Op }),
   ordersDB: makeOrdersDB({ model: Order }),
+  productsDB: makeProductsDB({ model: Product }),
 }
---
+++ infrastructure/store/sequelize/repositories/productsDB.js
@@ -0,0 +1,66 @@
+const { makeProduct } = require('../../../../domain/index')
+
+module.exports = function makeProductsDB({ model }) {
+  return Object.freeze({
+    async addProduct(product) {
+      await model.create({
+        id: product.id,
+        name: product.name,
+        type: product.type,
+        serial: product.serial,
+        imageUrl: product.imageUrl,
+        description: product.description,
+        isAvailable: product.isAvailable,
+        isBroken: product.isBroken,
+        freeDays: product.freeDays,
+        dayPrice: product.dayPrice,
+        createdAt: product.createdAt,
+        ownerId: product.ownerId,
+      })
+    },
+
+    async getProduct(id) {
+      const product = await model.findOne({
+        where: { id },
+      })
+      return makeProduct(product)
+    },
+
+    async updateProduct(product) {
+      await model.update(
+        {
+          id: product.id,
+          name: product.name,
+          type: product.type,
+          serial: product.serial,
+          imageUrl: product.imageUrl,
+          description: product.description,
+          isAvailable: product.isAvailable,
+          isBroken: product.isBroken,
+          freeDays: product.freeDays,
+          dayPrice: product.dayPrice,
+          createdAt: product.createdAt,
+          ownerId: product.ownerId,
+        },
+        {
+          where: {
+            id: product.id,
+          },
+        }
+      )
+    },
+
+    deleteProduct(productId) {
+      model.destroy({
+        where: {
+          id: productId,
+        },
+      })
+    },
+
+    async listProducts() {
+      const products = await model.findAll()
+      return products.map((p) => makeProduct(p))
+    },
+  })
+}
