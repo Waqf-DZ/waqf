6425c765b1d10456c9bc016522dac7c4f1a35246
Abdelaziz Mokhnache <m.abdelaziz1993@gmail.com>
2021-08-16 21:54:24 +0100

    create an ordersDB repository

--- infrastructure/store/sequelize/index.js
+++ infrastructure/store/sequelize/index.js
@@ -2,11 +2,13 @@ const sequelize = require('./sequelize')
 const { Op } = require('sequelize')
 
 const User = require('./models/user')
+const Order = require('./models/order')
 
 const makeUsersDB = require('./repositories/usersDB')
+const makeOrdersDB = require('./repositories/ordersDB')
 
 module.exports = {
   sequelize,
-
   usersDB: makeUsersDB({ model: User, Op }),
+  ordersDB: makeOrdersDB({ model: Order }),
 }
---
+++ infrastructure/store/sequelize/repositories/ordersDB.js
@@ -0,0 +1,70 @@
+const { makeOrder } = require('../../../../domain/index')
+
+module.exports = function makeOrdersDB({ model }) {
+  return Object.freeze({
+    async addOrder(order) {
+      await model.create({
+        id: order.id,
+        patientName: order.patientName,
+        patientAge: order.patientAge,
+        oxygenRatio: order.oxygenRatio,
+        hasChronicDesease: order.hasChronicDesease,
+        isCovid: order.isCovid,
+        prescriptionUrl: order.prescriptionUrl,
+        status: order.status,
+        assignedProductId: order.assignedProductId,
+        assignedUserId: order.assignedUserId,
+        createdAt: order.createdAt,
+        acceptedAt: order.acceptedAt,
+        completedAt: order.completedAt,
+        ownerId: order.ownerId,
+      })
+    },
+
+    async getOrder(id) {
+      const order = await model.findOne({
+        where: { id },
+      })
+      return makeOrder(order)
+    },
+
+    async updateOrder(order) {
+      await model.update(
+        {
+          id: order.id,
+          patientName: order.patientName,
+          patientAge: order.patientAge,
+          oxygenRatio: order.oxygenRatio,
+          hasChronicDesease: order.hasChronicDesease,
+          isCovid: order.isCovid,
+          prescriptionUrl: order.prescriptionUrl,
+          status: order.status,
+          assignedProductId: order.assignedProductId,
+          assignedUserId: order.assignedUserId,
+          createdAt: order.createdAt,
+          acceptedAt: order.acceptedAt,
+          completedAt: order.completedAt,
+          ownerId: order.ownerId,
+        },
+        {
+          where: {
+            id: order.id,
+          },
+        }
+      )
+    },
+
+    deleteOrder(orderId) {
+      model.destroy({
+        where: {
+          id: orderId,
+        },
+      })
+    },
+
+    async listOrders() {
+      const orders = await model.findAll()
+      return orders.map((o) => makeOrder(o))
+    },
+  })
+}
