f9a1e15056ab7fddc7fbd6007471af8fd736a0a9
Abdelaziz Mokhnache <m.abdelaziz1993@gmail.com>
2021-08-23 12:49:32 +0100

    help givers can only see pending or accepted by them orders

--- infrastructure/web/controllers/get-user-orders.js
+++ infrastructure/web/controllers/get-user-orders.js
@@ -1,10 +1,13 @@
 module.exports = function makeGetUserOrders({ listOrders }) {
   return async function getUserOrders(req, res) {
     try {
-      const ownerId = req.user.id
-      const ordersList = await listOrders({
-        ownerId: req.user.isSeekingHelp ? ownerId : null,
-      })
+      const { id, isSeekingHelp, isAdmin } = req.user
+      let ordersList = await listOrders({ ownerId: isSeekingHelp ? id : null })
+      if (!isSeekingHelp && !isAdmin) {
+        ordersList = ordersList.filter((o) => {
+          return o.isPending || o.assignedUserId == id
+        })
+      }
       res.render('profile/orders/index', { data: { orders: ordersList } })
     } catch (err) {
       res.render('profile/orders/index', { errorMessages: [err.message] })
